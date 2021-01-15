ecf9c412cd641e141a5689763ab0bd392241aaa9
emersonmorgado <silvestrevan@gmail.com>
2021-01-15 13:24:23 -0300

    Projeto criado com implementação do CRUD inicial

---
+++ src/main/java/br/com/emersonmorgado/forum/controller/TopicoController.java
@@ -0,0 +1,117 @@
+package br.com.emersonmorgado.forum.controller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.emersonmorgado.forum.controller.dto.DetalhesTopicoDto;
+import br.com.emersonmorgado.forum.controller.dto.TopicoDto;
+import br.com.emersonmorgado.forum.controller.form.AtualizacaoTopicoForm;
+import br.com.emersonmorgado.forum.controller.form.TopicoForm;
+import br.com.emersonmorgado.forum.modelo.Topico;
+import br.com.emersonmorgado.forum.repository.CursoRepository;
+import br.com.emersonmorgado.forum.repository.TopicoRepository;
+
+@RestController
+@RequestMapping("/topicos")
+public class TopicoController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
+	@Autowired
+	private CursoRepository cursoRepository;
+	
+	@GetMapping
+	public List<TopicoDto> lista(String nomeCurso){
+		System.out.println("nomeCurso");
+		if(nomeCurso==null) {
+			List<Topico> topicos = topicoRepository.findAll(); 
+			return TopicoDto.converter(topicos);
+		}else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+	}
+	
+	@PostMapping
+	@Transactional
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
+		Topico topico = form.cadastrar(cursoRepository);
+		topicoRepository.save(topico);
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+	
+//	@GetMapping("/{id}")
+//	public ResponseEntity<DetalhesTopicoDto> detalhar(@PathVariable Long id) {
+//		
+//		Optional<Topico> topico = topicoRepository.findById(id);
+//		if(topico.isPresent()) {
+//			return ResponseEntity.ok(new DetalhesTopicoDto(topico.get()));
+//		}
+//		return ResponseEntity.notFound().build();		
+//	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<DetalhesTopicoDto> detalhar(@PathVariable String id) {
+		try {
+			Optional<Topico> topico = topicoRepository.findById(Long.valueOf(id));
+			if(topico.isPresent()) {
+				return ResponseEntity.ok(new DetalhesTopicoDto(topico.get()));
+			}
+			return ResponseEntity.notFound().build();
+		} catch (Exception e) {
+			return ResponseEntity.notFound().build();
+		}		
+	}
+	
+	@PutMapping("/{id}")	
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
+		Optional<Topico> optionalTopico = topicoRepository.findById(id);
+		if(optionalTopico.isPresent()) {
+			Topico topico = form.atualizarComTopico(optionalTopico.get());
+			return ResponseEntity.ok(new TopicoDto(topico));
+		}
+		return ResponseEntity.notFound().build();	
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id){
+		if(topicoRepository.existsById(id)) {
+			topicoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/br/com/emersonmorgado/forum/controller/form/TopicoForm.java
@@ -0,0 +1,45 @@
+package br.com.emersonmorgado.forum.controller.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.emersonmorgado.forum.modelo.Curso;
+import br.com.emersonmorgado.forum.modelo.Topico;
+import br.com.emersonmorgado.forum.repository.CursoRepository;
+
+public class TopicoForm {
+
+	@NotNull @NotEmpty @Length(min = 5)
+	private String titulo;
+	
+	@NotNull @NotEmpty @Length(min = 10)
+	private String mensagem;
+	
+	@NotNull @NotEmpty
+	private String nomeCurso;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	public Topico cadastrar(CursoRepository cursoRepository) {
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		return new Topico(titulo, mensagem, curso);
+	}
+}
